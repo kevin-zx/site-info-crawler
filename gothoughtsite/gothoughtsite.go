11c6d47d0a65aaad7229ddc258b375c5fb9596eb
kevin-zx <Zx@^240228>
2019-07-11 17:04:51 +0800

    第一次提交

---
+++ gothoughtsite/gothoughtsite.go
@@ -0,0 +1,214 @@
+package gothoughtsite
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gocolly/colly"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+)
+
+type WebPageSeoInfo struct {
+	Title       string
+	Description string
+	Keywords    string
+	RealUrl     *url.URL
+	RecordCount int
+	InitUrl     string
+	Url         url.URL
+}
+
+type SiteLinkInfo struct {
+	AbsURL         string
+	StatusCode     int
+	ParentURL      string
+	Depth          int
+	WebPageSeoInfo *WebPageSeoInfo
+	H1             string
+	IsCrawler      bool
+	InnerText      string
+}
+
+var mu sync.Mutex
+
+func (wpsi *WebPageSeoInfo) SpiltKeywordsStr2Arr() (keywords []string) {
+	// 处理keywordStr 到arr
+	keywordsStr := wpsi.Keywords
+	//替换统一的分隔符
+	keywordsStr = strings.Replace(keywordsStr, ",", "|", -1)
+	keywordsStr = strings.Replace(keywordsStr, "-", "|", -1)
+	keywordsStr = strings.Replace(keywordsStr, "，", "|", -1)
+	keywordsStr = strings.Replace(keywordsStr, "、", "|", -1)
+	keywordsStr = strings.Replace(keywordsStr, "_", "|", -1)
+	keywordsStr = strings.Replace(keywordsStr, " ", "|", -1)
+	keywordsStr = strings.Replace(keywordsStr, "\t", "|", -1)
+	keywordsStr = strings.Replace(keywordsStr, ";", "|", -1)
+	keywordsStr = strings.Replace(keywordsStr, "；", "|", -1)
+
+	keywordsStr = strings.Replace(keywordsStr, "\n", "", -1)
+	keywordsStr = strings.Replace(keywordsStr, "“", "", -1)
+	keywordsStr = strings.Replace(keywordsStr, "”", "", -1)
+	keywords = removeDuplicatesAndEmpty(strings.Split(keywordsStr, "|"))
+	if len(keywordsStr) > 0 && len(keywords) == 1 {
+		fmt.Println("Package sitetools.comm.site_base Class WebPageSeoInfo function SplitKeywordsStr2Arr 遇到疑似解析失败的关键词" + keywordsStr)
+	}
+	return
+}
+
+func RunWithParams(siteUrlRaw string, limitCount int, timeout time.Duration, port int) (linkMap map[string]*SiteLinkInfo, err error) {
+	mu = sync.Mutex{}
+	linkMap = map[string]*SiteLinkInfo{siteUrlRaw: {}}
+	err = goThoughtSite(siteUrlRaw, port, limitCount, timeout, func(html *colly.HTMLElement) {
+		wi, err := parseWebSeoElement(html.DOM)
+		if err != nil {
+			return
+		}
+
+		currentUrl := html.Request.URL.String()
+
+		h1 := html.DOM.Find("h1")
+		mu.Lock()
+		if _, ok := linkMap[currentUrl]; !ok {
+			linkMap[currentUrl] = &SiteLinkInfo{AbsURL: currentUrl}
+		}
+
+		linkMap[currentUrl].InnerText = html.DOM.Find("body").Text()
+		//fmt.Println(linkMap[currentUrl].InnerText)
+		TextLen := len(strings.Split(linkMap[currentUrl].InnerText, ""))
+		if TextLen > 8000 {
+			TextLen = 8000
+		}
+
+		linkMap[currentUrl].InnerText = strings.Join(strings.Split(linkMap[currentUrl].InnerText, "")[0:TextLen], "")
+		//fmt.Println(linkMap[currentUrl].InnerText )
+		linkMap[currentUrl].IsCrawler = true
+		linkMap[currentUrl].H1 = clear(h1.Text())
+		linkMap[currentUrl].WebPageSeoInfo = wi
+		//charset,_ := html.DOM.Find("meta[http-equiv=Content-Type]").Attr("content")
+		//if strings.Contains(strings.ToLower(charset),"gb") {
+		//	convertGBKCharset(linkMap[currentUrl])
+		//}
+		linkMap[currentUrl].Depth = html.Request.Depth
+		if html.Response.StatusCode != 200 {
+			fmt.Println(html.Response.StatusCode)
+		}
+		linkMap[currentUrl].StatusCode = html.Response.StatusCode
+		mu.Unlock()
+	}, func(response *colly.Response, e error) {
+		mu.Lock()
+
+		currentUrl := response.Request.URL.String()
+		if _, ok := linkMap[currentUrl]; !ok {
+			linkMap[currentUrl] = &SiteLinkInfo{AbsURL: currentUrl}
+		}
+		existLink := linkMap[currentUrl]
+		fmt.Println(existLink.StatusCode)
+		if !linkMap[currentUrl].IsCrawler {
+			linkMap[currentUrl].IsCrawler = true
+			linkMap[currentUrl].Depth = response.Request.Depth
+			linkMap[currentUrl].StatusCode = response.StatusCode
+		}
+
+		mu.Unlock()
+	}, func(currentUrl string, parentUrl string) {
+		mu.Lock()
+		if _, ok := linkMap[currentUrl]; !ok {
+			linkMap[currentUrl] = &SiteLinkInfo{AbsURL: currentUrl}
+		}
+		linkMap[currentUrl].ParentURL = parentUrl
+		mu.Unlock()
+	})
+	for k, v := range linkMap {
+		// todo:会有absUrl为空的情况，暂时搞不懂为什么。先暴力修复
+		if v.AbsURL == "" {
+			linkMap[k].AbsURL = k
+		}
+		if !v.IsCrawler {
+			delete(linkMap, k)
+		}
+	}
+	return
+}
+
+func convertGBKCharset(sli *SiteLinkInfo) {
+	h1B, err := gbkToUtf8([]byte(sli.H1))
+	if err == nil {
+		sli.H1 = string(h1B)
+	}
+	innerTextByte, err := gbkToUtf8([]byte(sli.InnerText))
+	if err == nil {
+		fmt.Println(sli.InnerText)
+		sli.InnerText = string(innerTextByte)
+		fmt.Println(sli.InnerText)
+	}
+	descByte, err := gbkToUtf8([]byte(sli.WebPageSeoInfo.Description))
+	if err == nil {
+		sli.WebPageSeoInfo.Description = string(descByte)
+	}
+	keywordsByte, err := gbkToUtf8([]byte(sli.WebPageSeoInfo.Keywords))
+	if err == nil {
+		sli.WebPageSeoInfo.Keywords = string(keywordsByte)
+	}
+
+	TitleByte, err := gbkToUtf8([]byte(sli.WebPageSeoInfo.Title))
+	if err == nil {
+		sli.WebPageSeoInfo.Title = string(TitleByte)
+	}
+
+}
+
+func gbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GB18030.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+func clear(s string) string {
+	s = strings.TrimSpace(s)
+	s = strings.Trim(s, " ")
+	s = strings.Replace(s, "​", "", -1)
+	s = strings.Replace(s, "\n", "", -1)
+	return s
+}
+
+func parseWebSeoElement(html *goquery.Selection) (*WebPageSeoInfo, error) {
+
+	title := html.Find("title").Text()
+	description, _ := html.Find("meta[name=description]").Attr("content")
+	keywords, _ := html.Find("meta[name=keywords]").Attr("content")
+	site := WebPageSeoInfo{Title: title, Description: description, Keywords: keywords}
+	return &site, nil
+}
+
+func removeDuplicatesAndEmpty(a []string) (ret []string) {
+	var keywordCount = make(map[string]int)
+	a_len := len(a)
+	for i := 0; i < a_len; i++ {
+		duFlag := false
+		for _, re := range ret {
+			if len(a[i]) == 0 {
+				duFlag = true
+				break
+			}
+			if re == a[i] {
+				if _, ok := keywordCount[re]; !ok {
+					keywordCount[re] = 1
+				}
+				duFlag = true
+				break
+			}
+		}
+		if !duFlag {
+			ret = append(ret, a[i])
+		}
+	}
+	return
+}
---
+++ gothoughtsite/thoughtbase.go
@@ -0,0 +1,132 @@
+package gothoughtsite
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gocolly/colly"
+	"net/url"
+	"regexp"
+	"strings"
+	"time"
+)
+
+const fileRegString = ".+?(\\.jpg|\\.png|\\.gif|\\.GIF|\\.PNG|\\.JPG|\\.pdf|\\.PDF|\\.doc|\\.DOC|\\.csv|\\.CSV|\\.xls|\\.XLS|\\.xlsx|\\.XLSX|\\.mp40|\\.lfu|\\.DNG|\\.ZIP|\\.zip)(\\W+?\\w|$)"
+
+func goThoughtSite(siteUrlStr string, port int, limitCount int, timeOut time.Duration, handler func(html *colly.HTMLElement), onErr func(response *colly.Response, e error), parentInfo func(currentUrl string, parentUrl string)) (err error) {
+	//userAgent := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+
+	userAgent := "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)"
+	if port == 2 {
+		userAgent = "Mozilla/5.0 (Linux;u;Android 4.2.2;zh-cn;) AppleWebKit/534.46 (KHTML,like Gecko) Version/5.1 Mobile Safari/10600.6.3 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html）"
+	}
+	siteUrl, err := url.Parse(siteUrlStr)
+	if err != nil {
+		return err
+	}
+
+	c := colly.NewCollector(
+		colly.AllowedDomains(siteUrl.Host),
+		colly.DisallowedURLFilters(regexp.MustCompile(fileRegString)),
+		colly.UserAgent(userAgent),
+		colly.Async(true),
+		colly.MaxDepth(1000),
+	)
+	c.DetectCharset = true
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*" + siteUrl.Host + "*",
+		Parallelism: 4,
+		RandomDelay: timeOut,
+		Delay:       timeOut,
+	})
+	c.SetRequestTimeout(20 * time.Second)
+	c.OnHTML("html", func(e *colly.HTMLElement) {
+
+		fmt.Println(e.Request.ID)
+		if e.Request.ID%50 == 0 {
+			fmt.Printf("爬取了 %d 个\n", e.Request.ID)
+		}
+		if handler != nil {
+			handler(e)
+		}
+
+		e.DOM.Find("a[href]").Each(func(i int, a *goquery.Selection) {
+			href, ok := a.Attr("href")
+			if !ok {
+				return
+			}
+			link := clearUrl(href)
+			resultUrl := parseUrl(e.Request.URL, link)
+
+			//if resultUrl != "" && (strings.HasPrefix(resultUrl, "http:") || strings.HasPrefix(resultUrl, "https:")) && !strings.HasPrefix(link, "./") && !strings.HasPrefix(link, "/") && !strings.HasPrefix(link, "http") {
+			//	link = "/" + link
+			//	resultUrl = parseUrl(e.Request.URL, link)
+			//}
+			if resultUrl != "" {
+				erri := e.Request.Visit(resultUrl)
+				if erri == nil {
+					parentInfo(resultUrl, e.Request.URL.String())
+				}
+			}
+		})
+	})
+	c.OnRequest(func(request *colly.Request) {
+		if request.ID > uint32(limitCount) {
+			request.Abort()
+		}
+		//request.Headers.Add("Accept-Encoding","gzip, deflate")
+		//
+		//request.Headers.Add("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+		//request.Headers.Add("Accept-Encoding","gzip, deflate")
+		//request.Headers.Add("Accept-Language","en,zh-CN;q=0.9,zh;q=0.8,en-US;q=0.7,zh-TW;q=0.6")
+		//request.Headers.Add("Connection","keep-alive")
+		//request.Headers.Add("Host","www.oubeisiman.com")
+		//request.Headers.Add("Referer","http://www.oubeisiman.com/pd.jsp?id=133")
+		//request.Headers.Add("Upgrade-Insecure-Requests","1")
+	})
+	c.OnResponse(func(response *colly.Response) {
+		if !strings.Contains(strings.ToLower(response.Headers.Get("Content-Type")), "html") {
+			response.Headers.Set("Content-Type", "text/html;")
+		}
+
+	})
+	c.OnError(onErr)
+	err = c.Visit(siteUrl.String())
+	c.Wait()
+	return err
+}
+
+func parseUrl(curl *url.URL, href string) string {
+	testUrl, _ := curl.Parse(href)
+	testUrlStr := ""
+	if testUrl != nil {
+		testUrlStr = testUrl.String()
+	}
+	return testUrlStr
+}
+
+func clearUrl(webUrl string) string {
+
+	webUrl = handlerDoubleSlant(webUrl)
+	//去除空格
+	webUrl = strings.TrimSpace(webUrl)
+	//utf-8空格
+	for strings.HasSuffix(webUrl, "%20") {
+		webUrl = string(webUrl[0 : len(webUrl)-3])
+	}
+
+	//unicode空格
+	webUrl = strings.Replace(webUrl, "&#10;", "", -1)
+	webUrl = strings.Replace(webUrl, "&#9;", "", -1)
+	//if !strings.HasPrefix(webUrl,"./") && !strings.HasPrefix(webUrl,"/") && !strings.HasPrefix(webUrl,"http"){
+	//	webUrl = "/"+webUrl
+	//}
+	return webUrl
+
+}
+
+func handlerDoubleSlant(webUrl string) string {
+	for strings.HasSuffix(webUrl, "//") {
+		webUrl = strings.Replace(webUrl, "//", "/", -1)
+	}
+	return webUrl
+}
