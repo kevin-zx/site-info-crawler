99b7c3437835be4aea3dc9cc586f00a64153f356
kevin-zx <zhangxian887@gmail.com>
2020-06-10 11:55:08 +0800

    add a example

--- crawler_test.go
+++ crawler_test.go
@@ -1,1 +1,20 @@
 package site_info_crawler
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/kevin-zx/site-info-crawler/sitethrougher"
+	"os"
+)
+
+func ExampleRunSiteWithCache() {
+	si, err := RunSiteWithCache("http://www.jianshu.com", os.TempDir(), 24*1, sitethrougher.DefaultOption)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("suffix:%s", si.Suffix)
+	for _, link := range si.SiteLinks {
+		data, _ := json.Marshal(link)
+		fmt.Sprintln(string(data))
+	}
+}
